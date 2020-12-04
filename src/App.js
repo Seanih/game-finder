2c3ce91da630ac3fe2f123eb124aa0414eab2a02
Sean Anih <sean.anih@gmail.com>
2020-12-04 11:01:25 -0500

    fixed error where state wasn't loading and finished Redux setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './pages/Home';
+
+function App() {
+	
+	return (
+		<div className='App'>
+			<h1>RAWG API App</h1>
+			<Home />
+		</div>
+	);
+}
+
+export default App;
